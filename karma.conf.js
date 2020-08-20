15c3b4ff52c2c239d7ae56ab0ebdbbe8f73565c4
pmisiek <69692116+pmisiek@users.noreply.github.com>
2020-08-20 12:16:44 +0200

    set to ChromeHeadless

--- karma.conf.js
+++ karma.conf.js
@@ -25,8 +25,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
+    customLaunchers:{
+      HeadlessChrome:{
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox'],
     restartOnFileChange: true
   });
 };
